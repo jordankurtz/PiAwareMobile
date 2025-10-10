d133e0d84bc4c523cafe7faa63519596712dfb7d
Jordan Kurtz <jordan@jordankurtz.com>
2025-10-10 10:22:39 -0500

    Add settings for restoring map state and centering map on start

--- composeApp/src/commonMain/kotlin/com/jordankurtz/piawaremobile/settings/Settings.kt
+++ composeApp/src/commonMain/kotlin/com/jordankurtz/piawaremobile/settings/Settings.kt
@@ -3,4 +3,6 @@ package com.jordankurtz.piawaremobile.settings
 data class Settings (
     val servers: List<Server>,
     val refreshInterval: Int,
+    val centerMapOnUserOnStart: Boolean,
+    val restoreMapStateOnStart: Boolean
 )
