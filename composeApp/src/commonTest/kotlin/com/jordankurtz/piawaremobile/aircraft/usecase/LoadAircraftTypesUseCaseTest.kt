868962147eed155cf701527801b7b573e48b3f23
Jordan Kurtz <jordan@jordankurtz.com>
2025-10-28 18:52:47 -0500

    Add tests for LoadAircraftTypesUseCase

---
+++ composeApp/src/commonTest/kotlin/com/jordankurtz/piawaremobile/aircraft/usecase/LoadAircraftTypesUseCaseTest.kt
@@ -0,0 +1,32 @@
+package com.jordankurtz.piawaremobile.aircraft.usecase
+
+import com.jordankurtz.piawaremobile.aircraft.repo.AircraftRepo
+import dev.mokkery.answering.returns
+import dev.mokkery.everySuspend
+import dev.mokkery.mock
+import dev.mokkery.verifySuspend
+import kotlinx.coroutines.test.runTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class LoadAircraftTypesUseCaseTest {
+
+    private lateinit var aircraftRepo: AircraftRepo
+    private lateinit var useCase: LoadAircraftTypesUseCase
+
+    @BeforeTest
+    fun setup() {
+        aircraftRepo = mock()
+        useCase = LoadAircraftTypesUseCase(aircraftRepo)
+    }
+
+    @Test
+    fun `invoke calls repo to load aircraft types`() = runTest {
+        val servers = listOf("server1", "server2")
+        everySuspend { aircraftRepo.loadAircraftTypes(servers) } returns Unit
+
+        useCase(servers)
+
+        verifySuspend { aircraftRepo.loadAircraftTypes(servers) }
+    }
+}
