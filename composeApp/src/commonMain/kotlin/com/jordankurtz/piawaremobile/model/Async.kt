153c854306bf3bbe6a2cd8f489da5f886f2dcfa4
Jordan Kurtz <jordan@jordankurtz.com>
2025-10-28 18:52:48 -0500

    Make Success in Async a data class

--- composeApp/src/commonMain/kotlin/com/jordankurtz/piawaremobile/model/Async.kt
+++ composeApp/src/commonMain/kotlin/com/jordankurtz/piawaremobile/model/Async.kt
@@ -4,7 +4,7 @@ package com.jordankurtz.piawaremobile.model
 sealed class Async<out T> {
     object NotStarted : Async<Nothing>()
     object Loading : Async<Nothing>()
-    class Success<T>(val data: T) : Async<T>()
+    data class Success<T>(val data: T) : Async<T>()
     data class Error(val message: String, val throwable: Throwable? = null) : Async<Nothing>()
 
     fun getValue(): T? {
