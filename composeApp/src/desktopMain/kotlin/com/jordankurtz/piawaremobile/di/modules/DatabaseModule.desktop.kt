dfeabc4f4a0319c6046d829f824bbc4c37b9c3e6
Jordan Kurtz <jordan@jordankurtz.com>
2026-04-04 14:19:01 -0500

    Wire OfflineTileStore into DatabaseModule on all platforms
    Registers SqlDelightOfflineTileStore as a Koin @Single in DatabaseModule
    so the interface is injectable across the app. All four platform-specific
    DatabaseModule files (common, Android, Desktop, iOS) are updated to
    include the new binding.

--- composeApp/src/desktopMain/kotlin/com/jordankurtz/piawaremobile/di/modules/DatabaseModule.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/jordankurtz/piawaremobile/di/modules/DatabaseModule.desktop.kt
@@ -1,7 +1,11 @@
 package com.jordankurtz.piawaremobile.di.modules
 
+import com.jordankurtz.piawaremobile.di.annotations.IODispatcher
 import com.jordankurtz.piawaremobile.map.cache.DatabaseDriverFactory
 import com.jordankurtz.piawaremobile.map.cache.TileCacheDatabase
+import com.jordankurtz.piawaremobile.map.offline.OfflineTileStore
+import com.jordankurtz.piawaremobile.map.offline.SqlDelightOfflineTileStore
+import kotlinx.coroutines.CoroutineDispatcher
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
 
@@ -13,4 +17,10 @@ actual class DatabaseModule {
         val driverFactory = DatabaseDriverFactory(dbDir, "piaware_mobile.db")
         return TileCacheDatabase(driverFactory.createDriver())
     }
+
+    @Single
+    actual fun provideOfflineTileStore(
+        database: TileCacheDatabase,
+        @IODispatcher ioDispatcher: CoroutineDispatcher,
+    ): OfflineTileStore = SqlDelightOfflineTileStore(database.tileCacheQueries, ioDispatcher)
 }
--- composeApp/src/iosMain/kotlin/com/jordankurtz/piawaremobile/di/modules/DatabaseModule.ios.kt
+++ composeApp/src/iosMain/kotlin/com/jordankurtz/piawaremobile/di/modules/DatabaseModule.ios.kt
@@ -1,7 +1,11 @@
 package com.jordankurtz.piawaremobile.di.modules
 
+import com.jordankurtz.piawaremobile.di.annotations.IODispatcher
 import com.jordankurtz.piawaremobile.map.cache.DatabaseDriverFactory
 import com.jordankurtz.piawaremobile.map.cache.TileCacheDatabase
+import com.jordankurtz.piawaremobile.map.offline.OfflineTileStore
+import com.jordankurtz.piawaremobile.map.offline.SqlDelightOfflineTileStore
+import kotlinx.coroutines.CoroutineDispatcher
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
 
@@ -12,4 +16,10 @@ actual class DatabaseModule {
         val driverFactory = DatabaseDriverFactory()
         return TileCacheDatabase(driverFactory.createDriver())
     }
+
+    @Single
+    actual fun provideOfflineTileStore(
+        database: TileCacheDatabase,
+        @IODispatcher ioDispatcher: CoroutineDispatcher,
+    ): OfflineTileStore = SqlDelightOfflineTileStore(database.tileCacheQueries, ioDispatcher)
 }
