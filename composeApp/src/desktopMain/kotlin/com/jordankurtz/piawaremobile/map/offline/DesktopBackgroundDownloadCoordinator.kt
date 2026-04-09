75f32edf7c599bc46236c77044d8c87fb5281fdc
Jordan Kurtz <jordan@jordankurtz.com>
2026-04-08 21:30:33 -0500

    Add PiAwareWidgets Widget Extension target

--- composeApp/src/desktopMain/kotlin/com/jordankurtz/piawaremobile/map/offline/DesktopBackgroundDownloadCoordinator.kt
+++ composeApp/src/desktopMain/kotlin/com/jordankurtz/piawaremobile/map/offline/DesktopBackgroundDownloadCoordinator.kt
@@ -2,6 +2,7 @@ package com.jordankurtz.piawaremobile.map.offline
 
 import com.jordankurtz.piawaremobile.di.annotations.IODispatcher
 import kotlinx.coroutines.CoroutineDispatcher
+
 class DesktopBackgroundDownloadCoordinator(
     engine: DownloadEngine,
     store: OfflineTileStore,
