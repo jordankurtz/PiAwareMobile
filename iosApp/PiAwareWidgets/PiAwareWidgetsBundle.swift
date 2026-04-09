142e05677b8b540d2a2a59e15b9230519e194726
Jordan Kurtz <jordan@jordankurtz.com>
2026-04-08 21:32:18 -0500

    Wire OfflineMapWidget into PiAwareWidgetsBundle

--- iosApp/PiAwareWidgets/PiAwareWidgetsBundle.swift
+++ iosApp/PiAwareWidgets/PiAwareWidgetsBundle.swift
@@ -1,19 +1,11 @@
-//
-//  PiAwareWidgetsBundle.swift
-//  PiAwareWidgets
-//
-//  Created by Jordan Kurtz on 4/8/26.
-//  Copyright © 2026 orgName. All rights reserved.
-//
-
-import WidgetKit
 import SwiftUI
+import WidgetKit
 
 @main
 struct PiAwareWidgetsBundle: WidgetBundle {
     var body: some Widget {
         PiAwareWidgets()
         PiAwareWidgetsControl()
-        PiAwareWidgetsLiveActivity()
+        OfflineMapWidget()
     }
 }
