fd68abd85f2032de4faccd56e202218464188f0f
Jordan Kurtz <jordan@jordankurtz.com>
2026-04-08 21:24:31 -0500

    Add DownloadStatus to OfflineMapActivityAttributes ContentState

--- iosApp/iosApp/OfflineMapActivityAttributes.swift
+++ iosApp/iosApp/OfflineMapActivityAttributes.swift
@@ -1,10 +1,17 @@
 import ActivityKit
 import Foundation
 
+enum DownloadStatus: String, Codable, Hashable {
+    case downloading
+    case complete
+    case failed
+}
+
 struct OfflineMapActivityAttributes: ActivityAttributes {
     struct ContentState: Codable, Hashable {
         var downloaded: Int
         var total: Int
+        var status: DownloadStatus
     }
 
     var regionName: String
